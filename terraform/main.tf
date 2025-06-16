db31396e9bce58518638e2da22fb892369a7d107
Drosos Katsimpras <katsimprasdr@gmail.com>
2025-06-17 02:39:35 +0300

    Add .gitignore file and updated main.tf and index.hml

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,5 @@
+// main.tf
+
 // Create the S3 bucket
 resource "aws_s3_bucket" "static_site" {
   bucket = var.bucket_name
@@ -8,7 +10,7 @@ resource "aws_s3_bucket" "static_site" {
   }
 }
 
-// Separate configuration for static website hosting (new method)
+// Configure the bucket for static website hosting (modern method)
 resource "aws_s3_bucket_website_configuration" "website_config" {
   bucket = aws_s3_bucket.static_site.id
 
@@ -19,4 +21,40 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
   error_document {
     key = "index.html"
   }
+}
+
+// Allow the bucket to be publicly accessible (required for static hosting)
+resource "aws_s3_bucket_public_access_block" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+// Attach a policy that allows public read access to objects in the bucket
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
+}
+
+// Upload the static index.html file to the bucket
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.static_site.id
+  key          = "index.html"
+  source       = var.index_file
+  content_type = "text/html"
 }
